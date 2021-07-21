5f4e556fcf959ad4143e78f6bd10a0b742c66a1b
Manuela Mag <manuelamag@192-168-0-104.rdsnet.ro>
2021-07-21 11:19:25 +0300

    crud operations on service level

--- insurance_company/src/main/java/com/example/insurance_company/services/IUserService.java
+++ insurance_company/src/main/java/com/example/insurance_company/services/IUserService.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface IUserService {
     List<User> findAll();
+    void addUser(User user);
+    void updateUser(User user);
+    void deleteUser(Long id);
+    User getUserById(Long id);
 }
